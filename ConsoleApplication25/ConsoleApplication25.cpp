9e50b40bcb77dc37f4ac6c29edb821f9d7460122
std <std@RED-STUDENT15>
2023-02-27 19:06:01 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication25/ConsoleApplication25.cpp
@@ -0,0 +1,31 @@
+﻿#include <string.h>
+#include <stdio.h>
+#include <iostream>
+#include <stdlib.h>
+using namespace std;
+#include <iostream>
+#include <cstring>
+void main() {
+
+
+
+    const int dig = 10;
+    char str[dig];
+    cin >> str;
+    cout << "Zaniato: " << strlen(str) << "\nSvobodno: " << dig - strlen(str) << std::endl;
+    int m = strlen(str) + 1, n = strlen(str) + 1;
+    while (m >= strlen(str) && n > strlen(str))
+    cin >> m >> n;
+
+    char* str1 = new char[n - m];
+    int i, j = 0;
+    for (i = m; i < n; i++) {
+        cout << str[i];
+        str1[j] = str[i];
+        j++;
+    }
+    cout << std::endl << str1;
+    delete[] str1;
+}
+
+
